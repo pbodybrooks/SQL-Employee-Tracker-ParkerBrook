49daaefc6494a48fe3bc3337688d88cdd6dd4dc2
Parker <parkerlamarbrook@gmail.com>
2023-05-14 17:46:41 -0600

    added ASCII splashscreen logo, created index.js and query.sql files in db folder.

---
+++ db/index.js
@@ -0,0 +1,46 @@
+function dbOperations(data) {
+    switch (data.userChoice) {
+        case "View All Departments":
+            viewAllDepartments();
+            break;
+        case "View All Roles":
+            viewAllRoles();
+            break;
+        case "View All Employees":
+            viewAllEmployees();
+            break;
+        case "Add a Department":
+            addDepartment();
+            break;
+        case "Add a Role":
+            addRole();
+            break;
+        case "Add an Employee":
+            addEmployee();
+            break;
+        case "Update an Employee's Role":
+            updateEmployeeRole();
+            break;
+        case "Update an Employee's Manager":
+            updateEmployeeManager();
+            break;
+        case "View Employees by Manager":
+            viewEmployeesByManager();
+            break;
+        case "View Employees by Department":
+            viewEmployeesByDepartment();
+            break;
+        case "Delete a Department":
+            deleteDepartment();
+            break;
+        case "Delete a Role":
+            deleteRole();
+            break;
+        case "Delete an Employee":
+            deleteEmployee();
+            break;
+        case "Quit":
+            quit();
+            break;
+    }
+}
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const inquirer = require('inquirer');
 
 const logo = require('asciiart-logo');
+const e = require('express');
 
 
 const userPrompts = [
@@ -26,3 +27,37 @@ const userPrompts = [
         ]
     }
 ];
+
+// renders splashscreen logo and initializes inquirer to prompt the user for a database action
+function init() {
+    renderASCIILogo();
+
+    inquirer.prompt(userPrompts).then(data => {
+        dbOperations(data.userChoice);
+    });
+};
+
+// utilizes the asciiart-logo package to render a splashscreen in the console
+function renderASCIILogo() {
+    const welcomeMessage = "Welcome to the Employee Management System! This application features an interface that allows you to view and manage the departments, roles, and employees in your company. To begin, simply select an action below and follow the prompts. Enjoy!"
+
+    console.log(logo({ 
+        name: "Employee Management System!",
+        font: "DOS Rebel",
+        lineChars: 8,
+        padding: 2,
+        margin: 2,
+        borderColor: 'bold-blue',
+        logoColor: 'blue',
+        textColor: 'bold-white',
+    })
+    .emptyLine()
+    .right('Built with ♥ by Park')
+    .emptyLine()
+    .emptyLine()
+    .center(welcomeMessage)
+    .render());
+};
+
+// function call to initialize program
+init();
